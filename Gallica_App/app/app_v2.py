857ae36640565ae52193faa3bdd9d1fe49951cce
vincent.thong <vincent.thong@epita.fr>
2024-02-07 14:08:55 +0100

    Ajout de liste déroulante dynamique, partage d'information entre tabs , fin de l'interface attaquons la galerie

--- Gallica_App/app/app_v2.py
+++ Gallica_App/app/app_v2.py
@@ -1,64 +1,72 @@
 import os
 import gradio as gr
-from detectron2 import model_zoo
-from detectron2.config import get_cfg
-from gradio_utils import list_weight_files, create_summary_message, classes, default_classes
+from gradio_utils import list_weight_files, create_summary_message, \
+                classes, default_classes, initialize_predictors, layoutparser_weights_directory, detectron2_weights_directory
 
+# Available models for list
 models = ["Detectron2", "LayoutParser", "YOLOv8"]
-
-weight_dir = '../weights/'
-# Directories for the weight files
-layoutparser_weights_directory = weight_dir + 'LayoutParser/'
-detectron2_weights_directory = weight_dir + 'Detectron2/'
-
-default_array = [None, "Default", "", []]
-default_detectron2 = "COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"
-default_layoutP = "LP_weights.pth"
 # Get the list of weight files for both models
-layoutparser_weight_files = ["Default"] + list_weight_files(layoutparser_weights_directory)
-detectron2_weight_files = ["Default"] + list_weight_files(detectron2_weights_directory)
 
-# Define the main function that will use the selected options (updated to include Detectron2 weight path selection)
-def process_images(selected_models, detectron2_config, layoutparser_weight, class_selection, message_information):
-    message = create_summary_message(selected_models, detectron2_config, layoutparser_weight, class_selection)
-    if isinstance(detectron2_config, list):
-        detectron2_config = detectron2_config[0] if detectron2_config else "Default"
-        
-    if "Detectron2" in selected_models:
-        cfg_detectron2 = get_cfg()
-        if detectron2_config in default_array:
-            cfg_detectron2.merge_from_file(model_zoo.get_config_file(default_detectron2))
-            cfg_detectron2.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(default_detectron2)
-        else:
-            cfg_detectron2.MODEL.WEIGHTS = os.path.join(detectron2_weights_directory, detectron2_config)
-            
-    if "LayoutParser" in selected_models:
-        cfg_LayoutP = get_cfg()
-        cfg_LayoutP.merge_from_file('../models/LayoutParser/config.yml')
-        if layoutparser_weight in default_array:
-            cfg_LayoutP.MODEL.WEIGHTS = os.path.join(layoutparser_weights_directory, default_layoutP)
-        else:
-            cfg_LayoutP.MODEL.WEIGHTS = os.path.join(layoutparser_weights_directory, layoutparser_weight)
+def update_dropdowns():
+    layoutparser_weight_files = ["Default"] + list_weight_files(layoutparser_weights_directory)
+    detectron2_weight_files = ["Default"] + list_weight_files(detectron2_weights_directory)
+    LPdropDown = gr.Dropdown(choices=layoutparser_weight_files,  label="LayoutParser Weight File")
+    DT2dropDown = gr.Dropdown(choices=detectron2_weight_files , label="Detectron2 Weight File")
+    return LPdropDown,  DT2dropDown
     
-    return message, message
+# Define the main function that will use the selected options (updated to include Detectron2 weight path selection) in Gradio
+def process_images(selected_models, detectron2_weight,
+                   layoutparser_weight, class_selection, detection_threshold):
+    #create un message to display
+    message = create_summary_message(selected_models, detectron2_weight, 
+                                     layoutparser_weight, class_selection)
+    try:
+        # Setup predictors
+        predictors = initialize_predictors(selected_models, detectron2_weight, layoutparser_weight, detection_threshold, class_selection)
+        # If successful, you could append a success message or details about the predictors
+        message += "\nPredictors initialized successfully."
+    except Exception as e:
+        # Append error message to the message string
+        message += f"\nError initializing predictors: {str(e)}"
+        
+    return message
 
+def setup_model_selection_interface():
+    selected_models = gr.CheckboxGroup(choices=models, label="Select Model(s)", scale=2)
+    
+    lp_weights_files = ["Default"] + list_weight_files(layoutparser_weights_directory)
+    dt2_weight_files = ["Default"] + list_weight_files(detectron2_weights_directory)
+    layoutparser_weight = gr.Dropdown(choices=lp_weights_files, label="LayoutParser Weight File", allow_custom_value=True)
+    detectron2_weight = gr.Dropdown(choices=dt2_weight_files, label="Detectron2 Weight File", allow_custom_value=True)
+    ref_button = gr.Button("Refresh list", size='sm')
+                
+    class_selection = gr.Radio(choices=["Default Classes", "All Classes"], label="Class Selection", value="Default Classes", scale=2)
+    threshold_input = gr.Number(label="Detection Threshold", value=0.8, step=0.01, minimum=0.0, maximum=1.0, interactive=True)
+    
+    # Returning all components to include in the UI
+    return (selected_models, layoutparser_weight, detectron2_weight, ref_button, class_selection, threshold_input)
 # State is an class which can be used across blocks, tabs,...
-
+model_selection_components = setup_model_selection_interface()
 with gr.Blocks(title="Gallica BNF Benchmark Visualisation Tool by EPITA", theme=gr.themes.Soft()) as demo:
     message_information = gr.State([])
     with gr.Tab("Set Evaluation"):
         with gr.Row():
             with gr.Column():
-                # vairable correspond to a zone
-                selected_models = gr.CheckboxGroup(choices=models, label="Select Model(s)", scale=2)
-                layoutparser_weight = gr.Dropdown(choices=layoutparser_weight_files, label="LayoutParser Weight File")
-                detectron2_config = gr.Dropdown(choices=detectron2_weight_files, label="Detectron2 Weight File")
-                class_selection = gr.Radio(choices=["Default Classes", "All Classes"], label="Class Selection", value="Default Classes", scale=2)
-                btn = gr.Button("Process Images", size='sm')
-            with gr.Column():
-                info = gr.Text(label="Information", scale=3)
                 
-        btn.click(process_images, inputs=[selected_models, detectron2_config, layoutparser_weight, class_selection, message_information], outputs=[info, message_information])
+                selected_models, layoutparser_weight, detectron2_weight, ref_button, class_selection, threshold_input = setup_model_selection_interface()
+                ref_button.click(
+                    fn=update_dropdowns,
+                    inputs=[],
+                    outputs=[layoutparser_weight, detectron2_weight]
+                )
+                btn_process_images = gr.Button("Process Images", size='sm')
+                     
+            with gr.Column():
+                info = gr.Text(label="Information", scale=3)  
+                
+        btn_process_images.click(process_images, 
+                                 inputs=[selected_models, detectron2_weight, layoutparser_weight, class_selection, threshold_input], 
+                                 outputs=[info])  
     with gr.Tab("Metrics"):
         metrics_output = gr.Text(label="Information", scale=3)
         # This button is used to refresh the metrics tab with the latest state information.
@@ -66,5 +74,33 @@ with gr.Blocks(title="Gallica BNF Benchmark Visualisation Tool by EPITA", theme=
         def get_states(info):
             return info
         refresh_btn.click(get_states,inputs=message_information, outputs=metrics_output)
-
+        # add an upload of the report HTML static 
+    with gr.Tab("Gallery"):
+        # Galleries of each Models Maybe add a DropDown to select display
+        with gr.Column():
+             gt = gr.Gallery(label="GroundTruth")
+             det_gl = gr.Gallery(label="Detectron2")
+             lay_gl = gr.Gallery(label="LayoutParser")
+    with gr.Tab("Evaluate an Image"):
+        #TODO SAME AS above but not with ground truth , random Image
+         with gr.Row():
+            with gr.Column():
+                
+                selected_models, layoutparser_weight, detectron2_weight, ref_button, class_selection, threshold_input = setup_model_selection_interface()
+                ref_button.click(
+                    fn=update_dropdowns,
+                    inputs=[],
+                    outputs=[layoutparser_weight, detectron2_weight]
+                )
+                btn_process_images = gr.Button("Process Images", size='sm')
+                     
+            with gr.Column():
+                info = gr.Text(label="Information", scale=1)  
+                image_input = gr.Image(label="Drop Image", scale= 2)
+                image_output = gr.Image()
+            
+            btn_process_images.click(process_images, 
+                                 inputs=[selected_models, detectron2_weight, layoutparser_weight, class_selection, threshold_input], 
+                                 outputs=[info])  
+    
 demo.launch()
--- Gallica_App/app/gradio_utils.py
+++ Gallica_App/app/gradio_utils.py
@@ -1,4 +1,30 @@
 import os
+from PIL import Image, ImageDraw, ImageFont
+import json
+import torch
+import detectron2
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.modeling import build_model
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2 import model_zoo
+
+data_dir = '../data/' 
+weight_dir = '../weights/'
+gt_json_dir = data_dir + 'annotations/'
+img_dir = data_dir + 'images/'
+
+# Directories for the weight files
+layoutparser_weights_directory = weight_dir + 'LayoutParser/'
+detectron2_weights_directory = weight_dir + 'Detectron2/'
+
+# Preset Default Configuration
+default_array = [None, "Default", "", []]
+default_detectron2 = "COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"
+default_layoutP = "model_final.pth"
+# Json File with Ground Truth
+json_file = gt_json_dir + "gallicaimages_set1.json"
+
 # Preset classes and default classes
 classes = {
     'tampon': 0,
@@ -19,6 +45,25 @@ default_classes = {
     'photographie': 3,
     'estampe': 4
 }
+
+class_color_map = {
+            'nothing': "black",
+            'tampon': "blue",
+            'écriture manuscrite': "green",
+            'écriture typographique': "yellow",
+            'photographie': "purple",
+            'estampe': "orange",
+            'décoration': "pink",
+            'timbre': "red",
+            'dessin': "brown"
+        }
+def update_dropdown(folder_path):
+    # List all files in the folder
+    files = os.listdir(folder_path)
+    # Update the dropdown options dynamically
+    dropdown.update(choices=files)
+    return "Dropdown updated!"
+
 
 def list_weight_files(directory):
     """
@@ -45,9 +90,9 @@ def create_summary_message(selected_models, detectron2_config, layoutparser_weig
     # Handle LayoutParser model selection and configuration
     if "LayoutParser" in selected_models:
         if layoutparser_weight in  [None, "Default", "", []] :
-            message += "\n - LayoutParser with default configuration : LP_weights.pth"
+            message += "\n - LayoutParser with default configuration : model_final.pth"
         else:
-            message += f"\nLayoutParser with weight file: {layoutparser_weight}."
+            message += f"\n - LayoutParser with weight file: {layoutparser_weight}."
     if "YOLOv8" in selected_models:
         message += "\nYolov8 Not implemented"
     # Handle class selection
@@ -55,9 +100,43 @@ def create_summary_message(selected_models, detectron2_config, layoutparser_weig
     if class_selection == "Default Classes":
         used_classes = default_classes
         class_names = ", ".join(used_classes.keys())
-        message += f"\nUsing default classes: \n {class_names}."
+        message += f"\nUsing default classes: {len(default_classes)}\n{class_names}.\n"
     else:  # "All Classes"
         used_classes = classes
         class_names = ", ".join(used_classes.keys())
-        message += f"\nUsing all classes: \n {class_names}."
+        message += f"\nUsing all classes:{len(default_classes)}\n {class_names}.\n"
     return  message
+
+def get_detectron2_paths(config):
+    # Simplification: centralise la logique de configuration de Detectron2
+    if config in default_array:
+        return model_zoo.get_config_file(default_detectron2), model_zoo.get_checkpoint_url(default_detectron2)
+    else:
+        return os.path.join(detectron2_weights_directory, config), os.path.join(detectron2_weights_directory, config)
+
+def get_layoutparser_paths(weight):
+    # Simplification: centralise la logique de configuration de LayoutParser
+    if weight in default_array:
+        return '../models/LayoutParser/config.yml', os.path.join(layoutparser_weights_directory, default_layoutP)
+    else:
+        return '../models/LayoutParser/config.yml', os.path.join(layoutparser_weights_directory, weight)
+
+def initialize_predictors(selected_models, detectron2_weight, layoutparser_weight, detection_threshold, class_selection):
+    if isinstance(detectron2_weight, list):
+        detectron2_weight = detectron2_weight[0] if detectron2_weight else "Default"
+    predictors = {}
+    for model in selected_models:
+        cfg = get_cfg()
+        if model == "Detectron2":
+            config_path, weights_path = get_detectron2_paths(detectron2_weight)
+        elif model == "LayoutParser":
+            config_path, weights_path = get_layoutparser_paths(layoutparser_weight)
+        else:
+            continue
+        
+        cfg.merge_from_file(config_path)
+        cfg.MODEL.WEIGHTS = weights_path
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = detection_threshold
+        predictors[model] = DefaultPredictor(cfg)
+    return predictors
+    
